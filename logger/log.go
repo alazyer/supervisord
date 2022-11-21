4a50b45fb06f632f9315492e8bb39c7400e3231f
Yuliang Zhang <ylzhang@alauda.io>
2022-11-22 00:02:21 +0800

    file log with perm of 0640 instead of 0666

--- logger/log.go
+++ logger/log.go
@@ -95,7 +95,7 @@ func (l *FileLogger) openFile(trunc bool) error {
 		l.file, err = os.Create(l.name)
 	} else {
 		l.fileSize = fileInfo.Size()
-		l.file, err = os.OpenFile(l.name, os.O_RDWR|os.O_APPEND, 0666)
+		l.file, err = os.OpenFile(l.name, os.O_RDWR|os.O_APPEND, 0640)
 	}
 	if err != nil {
 		fmt.Printf("Fail to open log file --%s-- with error %v\n", l.name, err)
